39ae7e1cc16fec4b1932c5108b4a5845fbe33ac8
maoyao <onkkxx@163.com>
2022-11-03 12:20:55 +0800

    cut homeController.go

---
+++ mainFile/trollers/homeController.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	beego "github.com/beego/beego/v2/server/web"
+	godump "github.com/favframework/debug"
+	"public_utils/config"
+	"time"
+)
+
+type HomeController struct {
+	beego.Controller
+}
+
+func (c *HomeController) Get() {
+	if config.Debug {
+		config.Website.Title = "DEBUG-工具喵"
+	}
+
+	godump.Dump(config.Website)
+
+	c.Data["website"] = config.Website
+	c.Data["timestamp"] = time.Now().Unix()
+
+	c.TplName = "index.tpl"
+}
+
+func (c *HomeController) Google() {
+	c.Ctx.WriteString("google-site-verification: google965a213a981ca817.html")
+}
